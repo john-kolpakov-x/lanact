6d8770a329ce4cfdc862a61f68f996b335b8131b
pompei <john.kolpakov.x@gmail.com>
2023-02-19 07:46:57 +0600

    Разобрался в основах парсинга, но пока непонятно как парсить строки

---
+++ src/test/java/kz/pompei/lanact/gen/language/ActParsingTest.java
@@ -0,0 +1,29 @@
+package kz.pompei.lanact.gen.language;
+
+import com.intellij.testFramework.ParsingTestCase;
+import kz.pompei.lanact.parser.ActParserDefinition;
+
+public class ActParsingTest extends ParsingTestCase {
+  public ActParsingTest() {
+    super("", "act", new ActParserDefinition());
+  }
+
+  public void testFirst() {
+    doTest(true);
+  }
+
+  @Override
+  protected boolean skipSpaces() {
+    return true;
+  }
+
+  @Override
+  protected boolean includeRanges() {
+    return true;
+  }
+
+  @Override
+  protected String getTestDataPath() {
+    return "src/test/testData";
+  }
+}
